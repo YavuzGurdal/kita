d591225820cd15c5a1b7b1178a358b62165640b8
Yavuz <yavuz@Yavuz-MBP.local>
2023-08-26 21:03:17 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-// import CoinOrginal from './components/contentCoin/CoinOrginal';
 import Home from './components/Home';
 import Uberuns from './components/Uberuns';
 import Projekte from './components/Projekte';
 import Stellungangebote from './components/Stellungangebote';
 import Kontakt from './components/Kontakt';
-// import Coin15m from './components/contentCoin/coin15m/Coin15m';
-// import Coin4h from './components/contentCoin/coin4h/Coin4h';
-// import Coin5mUpDown from './components/contentCoin/coin5mUpDown/Coin5mUpDown';
-
 
 function App() {
   return (
